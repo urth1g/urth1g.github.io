61e92b0028daa967844d9b073c3844e5e01b5df8
Djordje Jevremovic <djordje3g@gmail.com>
2019-01-04 21:58:37 +0100

    Delete everything

--- app/components/Nav/List.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const List = styled.ul`
-  font-family: Georgia, Times, 'Times New Roman', serif;
-  padding-left:0
-`;
-
-export default List;
--- app/containers/ContactPage/Loadable.js
+++
@@ -1,7 +0,0 @@
-import loadable from 'loadable-components';
-
-import LoadingIndicator from 'components/LoadingIndicator';
-
-export default loadable(() => import('./index'), {
-  LoadingComponent: LoadingIndicator,
-});
